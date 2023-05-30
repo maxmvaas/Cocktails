c0a5c6769a337f4da8a4ecd0c599da86d2fac0ab
maxmv <vasilevmo@Icloud.com>
2023-05-30 22:25:34 +0300

    add libraries

---
+++ app/src/main/kotlin/com/pet/cocktails/App.kt
@@ -0,0 +1,7 @@
+package com.pet.cocktails
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
--- app/src/main/kotlin/com/pet/cocktails/MainActivity.kt
+++ app/src/main/kotlin/com/pet/cocktails/MainActivity.kt
@@ -1,24 +1,47 @@
 package com.pet.cocktails
 
-import android.graphics.ColorFilter
-import androidx.appcompat.app.AppCompatActivity
-
 import android.os.Bundle
-import androidx.core.content.ContextCompat
-import com.airbnb.lottie.LottieProperty
-import com.airbnb.lottie.SimpleColorFilter
-import com.airbnb.lottie.model.KeyPath
-import com.airbnb.lottie.value.LottieValueCallback
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.updateLayoutParams
 import com.pet.cocktails.databinding.ActivityMainBinding
+import com.pet.cocktails.databinding.LayoutProgressBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var progressView: View? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    fun showProgressLayout() {
+        val progressBinding = LayoutProgressBinding.inflate(layoutInflater)
+        progressView = progressBinding.root
+        progressView?.layoutParams = ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        binding.root.addView(progressView)
+        progressView?.updateLayoutParams<ConstraintLayout.LayoutParams> {
+            bottomToBottom = ConstraintSet.PARENT_ID
+            startToStart = ConstraintSet.PARENT_ID
+            topToTop = ConstraintSet.PARENT_ID
+            endToEnd = ConstraintSet.PARENT_ID
+        }
+    }
+
+    fun hideProgressLayout() {
+        binding.root.removeView(progressView)
+        progressView = null
     }
 }
