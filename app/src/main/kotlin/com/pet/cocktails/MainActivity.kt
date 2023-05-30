845f48263bae42290458c6eedcde96cd3faef460
maxmv <maxmvaas@gmail.com>
2023-05-30 22:55:05 +0400

    add lottie

--- app/src/main/kotlin/com/pet/cocktails/MainActivity.kt
+++ app/src/main/kotlin/com/pet/cocktails/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.pet.cocktails
 
+import android.graphics.ColorFilter
 import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.airbnb.lottie.LottieProperty
+import com.airbnb.lottie.SimpleColorFilter
+import com.airbnb.lottie.model.KeyPath
+import com.airbnb.lottie.value.LottieValueCallback
+import com.pet.cocktails.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
